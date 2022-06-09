148a762aea8d164f6c969985ea3560e3fdfa9fd2
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-06-09 16:56:39 -0300

    Update OutdatedSubState.hx

--- sourcebb/OutdatedSubState.hx
+++ sourcebb/OutdatedSubState.hx
@@ -30,6 +30,9 @@ class OutdatedSubState extends MusicBeatState
 			txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 			txt.screenCenter();
 			add(txt);
+			#if android
+                        addVirtualPad(NONE, A);
+                        #end
 		}
 		else
 		{
